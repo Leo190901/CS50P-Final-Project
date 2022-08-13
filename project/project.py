9cdf8ed64312a59d51aa06889b4dbaccaebabba5
Leon Prenkocevic <leonprenkocevic40@gmail.com>
2022-08-13 15:24:04 +0200

    create function for password specifications

--- project/project.py
+++ project/project.py
@@ -1,14 +1,43 @@
+import random
+
+passwords = {}
+
+
 def main():
+    random.seed()
     print("####################################")
-    print("###  Secure Password Generator   ###")
+    print("###  Secure Password Manager   ###")
     print("####################################")
     print()
-    input("Do you want your Password to contain numbers?[y/n] ")
 
-    input("Do you want your Password to contain alphabetical letters?[y/n] ")
+    create_new_passwd()
 
-    input(
-        "Do you want your Password to contain numbers special characters?[y/n] ")
+
+def create_new_passwd():
+    specs = get_passwd_specifications()
+    # print(specs)
+
+
+def get_passwd_specifications():
+    specs = {}
+    if input("Do you want your Password to contain numbers?[y/n] ").lower() == "y":
+        specs["num"] = True
+    else:
+        specs["num"] = False
+    if input("Do you want your Password to contain lowercase letters?[y/n] ").lower() == "y":
+        specs["lower"] = True
+    else:
+        specs["lower"] = False
+    if input("Do you want your Password to contain uppercase letters?[y/n] ").lower() == "y":
+        specs["upper"] = True
+    else:
+        specs["upper"] = False
+    if input("Do you want your Password to contain special characters?[y/n] ").lower() == "y":
+        specs["special"] = True
+    else:
+        specs["special"] = False
+
+    return specs
 
 
 main()

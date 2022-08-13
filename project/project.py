6e3d8ebf3a952bc7e2fea4dde4ec69f61363645d
Leon Prenkocevic <leonprenkocevic40@gmail.com>
2022-08-13 14:48:38 +0200

    changed basic structure of project

---
+++ project/project.py
@@ -0,0 +1,14 @@
+def main():
+    print("####################################")
+    print("###  Secure Password Generator   ###")
+    print("####################################")
+    print()
+    input("Do you want your Password to contain numbers?[y/n] ")
+
+    input("Do you want your Password to contain alphabetical letters?[y/n] ")
+
+    input(
+        "Do you want your Password to contain numbers special characters?[y/n] ")
+
+
+main()

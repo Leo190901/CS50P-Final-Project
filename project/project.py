7e818f1ac9e70c88c073c4e281b4c42511b9d02f
Leon Prenkocevic <leonprenkocevic40@gmail.com>
2022-08-15 09:39:50 +0200

    fixed the bug, within the create_passwd function

--- project/project.py
+++ project/project.py
@@ -48,13 +48,11 @@ def load_passwords():
     with open('passwords.csv') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            print(row)
             applications[row[0]] = {"username": row[1], "password": row[2]}
 
 
 def display_passwords():
     col_names = ["App", "Username", "Password"]
-    print(applications)
     values = [[name, *inner.values()] for name, inner in applications.items()]
     print(tabulate(values, headers=col_names, tablefmt="grid", showindex="always"))
 
@@ -79,7 +77,7 @@ def create_new_passwd():
     applications[appName] = {"username": userName, "password": passwd}
     #fieldNames = ["App", "Username", "Password"]
     values = [appName, userName, passwd]
-    with open('passwords.csv', 'a') as csvfile:
+    with open('passwords.csv', 'a',  newline='\n', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(values)
 
